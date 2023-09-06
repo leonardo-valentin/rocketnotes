77b2c25340c55760b1552674ff86bfa86e09b0fe
leonardo-valentin <leo.v.santosbr@gmail.com>
2023-09-06 17:26:24 -0300

    feat img and welcome message in header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,19 @@
-import { Container } from "./style";
+import { Container, Profile } from "./style";
 
 export function Header() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Profile>
+        <img
+          src="https://github.com/leonardo-valentin.png"
+          alt="user profile picture"
+        />
+
+        <div>
+          <span>Bem vindo,</span>
+          <strong>Leonardo Valentin</strong>
+        </div>
+      </Profile>
+    </Container>
+  );
 }
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -11,6 +11,33 @@ export const Container = styled.header`
   display: flex;
   justify-content: space-between;
   padding: 0 8rem;
+`;
 
-  background: red;
+export const Profile = styled.header`
+  display: flex;
+  align-items: center;
+
+  > img {
+    width: 5.6rem;
+    height: 5.6rem;
+    object-fit: contain;
+    border-radius: 50%;
+  }
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    margin-left: 1.6rem;
+    line-height: 2.4rem;
+
+    span {
+      font-size: 1.4rem;
+      color: ${({ theme }) => theme.COLORS.GRAY_100};
+    }
+
+    strong {
+      font-size: 1.8rem;
+      color: ${({ theme }) => theme.COLORS.WHITE};
+    }
+  }
 `;
