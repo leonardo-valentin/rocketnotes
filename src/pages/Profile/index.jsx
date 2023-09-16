2c45b9519fc2e17c0fdc08d7681bfb1e95550688
leonardo-valentin <leo.v.santosbr@gmail.com>
2023-09-16 13:30:55 -0300

    feat: Profile Page

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,37 @@
+import { FiArrowLeft, FiUser, FiLock, FiMail, FiCamera } from "react-icons/fi";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+import { Container, Form, Avatar } from "./style";
+
+export function Profile() {
+  return (
+    <Container>
+      <header>
+        <a href="#">
+          <FiArrowLeft />
+        </a>
+      </header>
+
+      <Form>
+        <Avatar>
+          <img
+            src="https://github.com/leonardo-valentin.png"
+            alt="Foto do usuário"
+          />
+
+          <label htmlFor="avatar">
+            <FiCamera />
+            <input id="avatar" type="file" />
+          </label>
+        </Avatar>
+
+        <Input placeholder="Nome" type="text" icon={FiUser} />
+        <Input placeholder="E-mail" type="text" icon={FiMail} />
+        <Input placeholder="Senha atual" type="password" icon={FiLock} />
+        <Input placeholder="Nova Senha" type="password" icon={FiLock} />
+
+        <Button title="Salvar" />
+      </Form>
+    </Container>
+  );
+}
