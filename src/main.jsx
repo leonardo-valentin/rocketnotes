0683413924b27de8a852925602b1bbe2891daf16
leonardo-valentin <leo.v.santosbr@gmail.com>
2023-09-16 15:00:31 -0300

    feat: page routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import GlobalStyles from "./styles/global";
 import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme";
-import { New } from "./pages/New";
+import { Routes } from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <New />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>
 );
